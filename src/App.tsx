c62e42ca925daf67c8693612d15550b8998744ae
Godwin Sei Essiam <164816138+seigodwin@users.noreply.github.com>
2026-06-18 17:05:10 +0000

    Add HabitForm and HabitList Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 
-import { Header } from "./Header"
+import { HabitForm } from "./Components/HabitForm"
+import { HabitList } from "./Components/HabitList"
+import { Header } from "./Components/Header"
 
 export default function(){
   return (
     <div className="max-w-2xl mx-auto p-4 flex flex-col
     gap-4">
       <Header />
+      <HabitForm />
+      <HabitList />
     </div>
   )
 
---
+++ src/Components/HabitList.tsx
@@ -0,0 +1,48 @@
+import { eachDayOfInterval, endOfWeek, format, startOfWeek } from "date-fns";
+import { Button } from "./Button";
+
+export function HabitList(){
+    const habits: Habit[] = [{id: "a" , name:"Read"}];
+
+    if(habits.length === 0){
+        return <p className="text-center text-zinc-500 py-12">
+            No habits yet. Add one to get started</p>
+    }
+
+    return<div className="flex flex-col gap-3">
+        {habits.map(h => (
+             <HabitItem habit={h}></HabitItem>
+        ))}
+    </div>
+}
+
+type Habit = {
+    id: string;
+    name: string;
+};
+
+type HabitItemProps = {
+    habit: Habit;
+};
+
+const visibleDates = eachDayOfInterval({start: startOfWeek(new Date()) , end: endOfWeek(new Date())})
+
+
+function HabitItem({habit}: HabitItemProps){
+    return <div className="rounded-xl bg-zinc-800 p-4">
+        <div className="flex items-center justify-between mb-3">
+            <div className="flex gap-3 items-center">
+                <span className="font-medium">{habit.name}</span>
+                <span className="text-sm text-amber-400">🔥 3</span>
+            </div>
+            <Button>Delete</Button>
+        </div>
+        <div className="flex gap-1.5">
+            {visibleDates.map( d => (
+                <Button key={d.toISOString()}>
+                    <span className="font-medium">{format(d , "EEE")}</span>
+                </Button>
+            ))}
+        </div>
+    </div>
+}
